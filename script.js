18f58a631ee2d813bc96d1ff511e21e84f5cc6ae
Maiia Vyshniakova <m.vishnyakova2002@gmail.com>
2025-09-04 17:09:26 +0300

    fixing contact form issue

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+emailjs.init("jJxcj0PryYSoWXzXt");
 function sendEmail() {
     let _message = document.getElementById("message").value;
     let _email = document.getElementById("email").value;
     let _subject = document.getElementById("subject").value;
     let _name = document.getElementById("name").value;
 
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (_email === "" || _name === "" || !emailRegex.test(_email))
         return;
     if ( _message === "") 
@@ -20,6 +21,13 @@ function sendEmail() {
     }
 
     emailjs.send("service_eyvtkxd","template_dhfp3b6", params).then(
-        alert("Message sent!")
+        function(response) {
+            console.log("SUCCESS!", response.status, response.text)
+            alert("Message sent!");
+        },
+        function(error) {
+            console.log("FAILED...", error);
+            alert("Failed to send message, please try again later");
+        }
     );
 }
