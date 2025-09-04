8a9844b9a96c6025423120875413b498df6dcea6
Maiia Vyshniakova <m.vishnyakova2002@gmail.com>
2025-09-04 17:57:25 +0300

    fixed contact form logic

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-emailjs.init("jJxcj0PryYSoWXzXt");
-function sendEmail() {
+const contactForm = document.getElementById("contact-form")
+const sendBtn = document.getElementById("send-btn");
+let isSending = false;
+function sendEmail(event) {
+    if(event) event.preventDefault();
+    if(!contactForm.checkValidity()) {
+        contactForm.reportValidity();
+        return;
+    }
+
+    if(isSending) return;
+    isSending = true;
+    
+    sendBtn.disabled = true;
+    sendBtn.innerText = "Sending";
+
+    emailjs.init("jJxcj0PryYSoWXzXt");
+
     let _message = document.getElementById("message").value;
     let _email = document.getElementById("email").value;
     let _subject = document.getElementById("subject").value;
     let _name = document.getElementById("name").value;
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (_email === "" || _name === "" || !emailRegex.test(_email))
+    if (_email === "" || _name === "" || !emailRegex.test(_email)) {
+        resetButton();
         return;
+    }
     if ( _message === "") 
         _message = "Please contact me! " + _email + ", " + _name;
     if (_subject === "")
@@ -24,10 +42,20 @@ function sendEmail() {
         function(response) {
             console.log("SUCCESS!", response.status, response.text)
             alert("Message sent!");
+            sendBtn.disabled = false;
+            resetButton();
         },
         function(error) {
             console.log("FAILED...", error);
             alert("Failed to send message, please try again later");
+            sendBtn.disabled = false;
+            resetButton();
         }
     );
+}
+
+function resetButton() {
+    isSending = false;
+    sendBtn.disabled = false;
+    sendBtn.innerText = "Send";
 }
